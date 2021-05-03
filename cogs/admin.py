0b9ac7745822e1b9ed85e6f09cad6b0dea231d63
syoch <hacksyoch@gmail.com>
2021-05-03 22:14:18 +0900

    update: impl switchEval

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,8 +1,19 @@
 from discord.ext import commands
+import state
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.group()
+    async def admin(self,ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('This command is needed subcommand.\n'+
+                           'subcommands are switchEval, calc and graph.')
+
+    @admin.command()
+    async def switchEval(self,ctx):
+        state.state.enabledEval ^= 1
 
     async def check_cog(self, ctx):
         if ctx.author.id=="524516049752686592":
