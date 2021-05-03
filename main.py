de8a25b2ae42127c382e04e2820a370787a025f2
syoch <hacksyoch@gmail.com>
2021-05-03 11:23:18 +0900

    update: move f2l

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import re
 from math import ceil
 import logging
 import sys
+import util
 default_importCache = sys.modules
 
 logging.basicConfig(
@@ -37,51 +38,6 @@ def abcfegogeg():
     c = b-_b
     __builtins__.__dict__[chr(a+_b*_c)+chr(a+_b*_b)+chr(b)+chr(a)+_b*chr(b)+chr(b-_b)](C, chr(b-_b)+chr(b+_a)+chr(b-_b*_c))(
         eval(chr(a-_a-_b*_b*_c)+chr((_b**(_b*_c)+_b**_c*_b)-_a)+chr(a-_b*(_c+_b**_c))+chr(a-_b**_c*_c-_b**_b)+chr(a-_a-_c**_b*_b)))
-
-# --------------------
-# Program utilities
-# --------------------
-
-
-def f2l(formula_, symbols_=["x"]):
-    """
-    formula_ type is str
-    symbols_ type is list or tuple,str
-
-    formula_ Example:3x x x/2 x^2
-    symbols_ Example:["x","y"]
-    lambda arguments have this func second argument 'symbols_'
-    """
-    symbols = list(symbols_)
-    formula = str(formula_)
-    formula = re.sub(r"\|\-?(.*)\|", r"abs(\1)", formula)
-    formula = formula.replace("^", "**")
-    formula = re.sub(r"log\(([^\)]*)\)", r"log(\1)", formula)
-    formula = re.sub(r"log\[([^\]]*)\]\(([^\)]*)\)",
-                     r"log(\2)/log(\1)", formula)
-    formula = formula.replace("asin", "arcsin")
-    formula = formula.replace("acos", "arccos")
-    formula = formula.replace("atan", "arctan")
-    formula = formula.replace("asinh", "arsinh")
-    formula = formula.replace("acosh", "arccosh")
-    formula = formula.replace("atanh", "arctanh")
-    for s in symbols:
-        formula = re.sub(rf"{s}\*\*(\d+)", rf"({s}**\1)", formula)
-        while True:
-            oldf = formula
-            formula = re.sub(rf"(\d+){s}", rf"(\1*{s})", formula)
-            formula = re.sub(rf"{s}\(", rf"{s}*(", formula)
-            formula = re.sub(rf"\){s}", rf")*{s}", formula)
-            formula = re.sub(rf"{s}{s}", rf"({s}*{s})", formula)
-            formula = re.sub(rf"\)\(", rf")*(", formula)
-
-            if formula == oldf:
-                break
-    formula = re.sub(rf"(\d)\(", rf"\1*(", formula)
-    return (formula, eval(
-        "lambda "+",".join(symbols)+" : "+formula,
-        numpy.__dict__, math.__dict__
-    ))
 
 
 client = discord.Client()
@@ -349,7 +305,7 @@ async def graph(sender, args):
             _formula.append(arg)
     formula = "".join(_formula)
     x = numpy.linspace(s, e, ceil(1000*(e-s)))
-    ff = f2l(formula)
+    ff = util.f2l(formula)
     f = ff[1]
     plt.figure()
     plt.title("f(x)="+formula)
@@ -367,7 +323,7 @@ async def graph(sender, args):
 
 async def calc(sender, formula):
     try:
-        await sender(str(f2l(formula, symbols_="")[1]()))
+        await sender(str(util.f2l(formula, symbols_="")[1]()))
     except Exception as ex:
         await sender(str(ex))
 
