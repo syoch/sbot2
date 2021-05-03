7d7c1b798a3af832e949078c5ae41e3762de7c2d
syoch <hacksyoch@gmail.com>
2021-05-03 23:31:04 +0900

    update: implement switchEval

--- cogs/admin.py
+++ cogs/admin.py
@@ -10,6 +10,13 @@ class Admin(commands.Cog):
         state.state.enabledEval ^= 1
         await ctx.send(f"state.enabledEval = {state.state.enabledEval}")
 
+    @commands.command()
+    async def reload(self,ctx,*,name:str):
+        a=self.bot.reload_extension(name)
+        ret=str(a)
+        if not ret: ret="None"
+        await ctx.send(ret)
+
     async def check_cog(self, ctx):
         if ctx.author.id=="524516049752686592":
             return True
