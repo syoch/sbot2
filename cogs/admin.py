445d252a390f73cbdecb55fa55924d58c192ff5a
syoch <hacksyoch@gmail.com>
2021-09-26 18:55:11 +0900

    update: add reloadall command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,4 @@
+import subprocess
 from discord.ext import commands
 import state
 
@@ -11,8 +12,7 @@ class Admin(commands.Cog):
         state.state.enabledEval ^= 1
         await ctx.send(f"state.enabledEval = {state.state.enabledEval}")
 
-    @commands.command()
-    async def reload(self, ctx, *, name: str):
+    def reload(self, name):
         try:
             a = self.bot.reload_extension(name)
         except Exception as ex:
@@ -20,13 +20,29 @@ class Admin(commands.Cog):
         ret = str(a)
         if not ret:
             ret = "None"
-        await ctx.send(ret)
+        return ret
+
+    @commands.is_owner()
+    @commands.command()
+    async def reloadall(self, ctx):
+        await ctx.send("updating source")
+        output = subprocess.check_output(["./scripts/update.sh"])
+        await ctx.send(output.decode("utf-8"))
+
+        await ctx.send("Reloading all cogs...")
+        extensions = list(self.bot.extensions.keys())
+        result = [
+            str(cog).ljust(10) + " -> " + self.reload(cog)
+            for cog in extensions
+            if cog != self.qualified_name
+        ]
+        await ctx.send("\n".join(["```", *result, "```"]))
 
     async def check_cog(self, ctx):
         if ctx.author.id == "524516049752686592":
             return True
         else:
-            ctx.send("This command is blocked( can use by syoch only )")
+            await ctx.send("Cannot invoke the command by you")
             return False
 
 
