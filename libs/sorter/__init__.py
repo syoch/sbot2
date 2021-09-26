253e7f50dcfddcc4082f1c66861926da1b74908d
syoch <hacksyoch@gmail.com>
2021-09-26 18:16:31 +0900

    update: change language key

--- libs/sorter/__init__.py
+++ libs/sorter/__init__.py
@@ -1,6 +1,6 @@
 from .header import sort as header_sort
 
 table = {
-    "header": ["c++", header_sort],
-    "include": ["c++", header_sort],
+    "header": ["cpp", header_sort],
+    "include": ["cpp", header_sort],
 }
