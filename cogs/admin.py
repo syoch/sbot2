7eac58e1da9482dcfba2f1ccb523923d26768cef
syoch <hacksyoch@gmail.com>
2021-09-26 18:23:00 +0900

    refactor: format admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,31 +1,34 @@
 from discord.ext import commands
 import state
 
+
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
-    async def switchEval(self,ctx):
+    async def switchEval(self, ctx):
         state.state.enabledEval ^= 1
         await ctx.send(f"state.enabledEval = {state.state.enabledEval}")
 
     @commands.command()
-    async def reload(self,ctx,*,name:str):
+    async def reload(self, ctx, *, name: str):
         try:
-            a=self.bot.reload_extension(name)
+            a = self.bot.reload_extension(name)
         except Exception as ex:
-            a=type(ex).__name__+": "+str(ex)
-        ret=str(a)
-        if not ret: ret="None"
+            a = type(ex).__name__+": "+str(ex)
+        ret = str(a)
+        if not ret:
+            ret = "None"
         await ctx.send(ret)
 
     async def check_cog(self, ctx):
-        if ctx.author.id=="524516049752686592":
+        if ctx.author.id == "524516049752686592":
             return True
         else:
             ctx.send("This command is blocked( can use by syoch only )")
             return False
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
