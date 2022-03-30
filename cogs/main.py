5e0953a9d13d5ca56438f61eb836e4b065dd2bbe
syoch <hacksyoch@gmail.com>
2022-03-31 08:34:31 +0900

    update: Editted main cog

--- cogs/main.py
+++ cogs/main.py
@@ -1,9 +1,7 @@
 from discord.ext import commands
-import discord
-
 import logging
+from .. import prefix
 
-import state
 
 class Main(commands.Cog):
     def __init__(self, bot):
@@ -13,42 +11,10 @@ class Main(commands.Cog):
     async def good_reaction(self, message):
         if message.author.bot:
             return
-        if message.content.startswith("sb@"):
-            logging.info(f"command has called by {message.author.name}: {message.content}")
-            
-    @commands.command()
-    async def oldHelp(self, ctx):
-        await ctx.send(
-            "```\n"
-            r"Sbot v2 help"+"\n"
-            r"Sb<category>@<command> <arg...>"+"\n"
-            r""+"\n"
-            r"Categories"+"\n"
-            r"| [U]Utility"+"\n"
-            r"\ [G]General"+"\n"
-            r""+"\n"
-            r""+"\n"
-            r"Utilities help"+"\n"
-            r"| calc <formula:string>"+"\n"
-            r"| | Calculate formula"+"\n"
-            r"| \ ex. Sbu@calc 10^(log[10](100))"+"\n"
-            r"| "+"\n"
-            r"| graph <formula:string>"+"\n"
-            r"| | Draw a graph by formula"+"\n"
-            r"| \ ex. Sbu@graph sin(x)"+"\n"
-            r"| "+"\n"
-            r"| eval <laun:str> <program:str>"+"\n"
-            r"| | Evalute a program in arg with laun"+"\n"
-            r"| | Supported Languages"+"\n"
-            r"| | \ py"+"\n"
-            r"\ \ ex. Sbu@eval py 10**2"+"\n"
-            r""+"\n"
-            r""+"\n"
-            r"General help"+"\n"
-            r"| help"+"\n"
-            r"\ \show help"+"\n"
-            "```"
-        )
+        if message.content.startswith(prefix.get_prefix()):
+            logging.info(
+                f"command has called by {message.author.name}: {message.content}")
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
