e3810589c30fada0996b80f5448a994805c84498
syoch <hacksyoch@gmail.com>
2022-08-17 15:06:34 +0900

    Update: Rename codeblock to expr

--- bot/cogs/util.py
+++ bot/cogs/util.py
@@ -58,7 +58,7 @@ class Util(commands.Cog):
         ]))
 
     @commands.command(name="eval")
-    async def _eval(sender, ctx, *, codeblock: str):
+    async def _eval(self, ctx, *, expr: str):
         """
         Evalute python expression in safeeval
         """
@@ -67,7 +67,7 @@ class Util(commands.Cog):
             await ctx.send("eval is disabled")
             return
         try:
-            (ret, stdout) = safeeval._eval(codeblock)
+            (ret, stdout) = safeeval._eval(expr)
         except Exception as ex:
             import traceback
             await ctx.reply("Exception has occured!\n" +
@@ -77,7 +77,7 @@ class Util(commands.Cog):
             return
 
         # Create Content
-        code = codeblock.replace("```", "'''")
+        code = expr.replace("```", "'''")
 
         ret_len = len(ret)
         if ret_len >= 1500:
